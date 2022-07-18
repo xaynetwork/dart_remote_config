2bd3127d36db8d89ee547f1491e080d9763f99e0
michaelkmalak <michael.k.malak@gmail.com>
2022-07-18 10:43:45 +0200

    use hive in experimentation module

--- lib/experimentation/experimentation_module.dart
+++ lib/experimentation/experimentation_module.dart
@@ -1,6 +1,10 @@
 import 'package:dart_remote_config/experimentation/experimentation_engine.dart';
 import 'package:dart_remote_config/model/experimentation_engine_result.dart';
 import 'package:dart_remote_config/model/remote_config.dart';
+import 'package:hive/hive.dart';
+
+const _kExperimentationBoxKey = 'ExperimentationBox';
+const _kExperimentationResultKey = 'ExperimentationResult';
 
 abstract class ExperimentationModule {
   Future<ExperimentationEngineResult> fetch(RemoteConfig config);
@@ -17,13 +21,16 @@ class ExperimentsFetcherImpl implements ExperimentationModule {
     return newResult;
   }
 
-  Future<bool> writeResult(ExperimentationEngineResult result) async {
-    //TODO: write to local database
-    return true;
+  Future<void> writeResult(ExperimentationEngineResult result) async {
+    final box = await Hive.openLazyBox(_kExperimentationBoxKey);
+    await box.put(_kExperimentationResultKey, result.toJson());
   }
 
   Future<ExperimentationEngineResult?> readPreviousResult() async {
-    //TODO: read previous result from local database
-    return null;
+    final box = await Hive.openLazyBox(_kExperimentationBoxKey);
+    final jsonMap =
+        await box.get(_kExperimentationResultKey) as Map<String, dynamic>?;
+    if (jsonMap == null) return null;
+    return ExperimentationEngineResult.fromJson(jsonMap);
   }
 }
